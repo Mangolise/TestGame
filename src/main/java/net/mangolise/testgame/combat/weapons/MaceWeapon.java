b62d33bbc99113e49d939f95df5eeed4dc6a392b
EclipsedMango <140897468+EclipsedMango@users.noreply.github.com>
2025-07-19 11:21:52 +1000

    tweaked mace values

--- src/main/java/net/mangolise/testgame/combat/weapons/MaceWeapon.java
+++ src/main/java/net/mangolise/testgame/combat/weapons/MaceWeapon.java
@@ -9,16 +9,17 @@ import net.minestom.server.MinecraftServer;
 import net.minestom.server.component.DataComponents;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.coordinate.Vec;
-import net.minestom.server.entity.Entity;
-import net.minestom.server.entity.EntityType;
-import net.minestom.server.entity.LivingEntity;
-import net.minestom.server.entity.Player;
+import net.minestom.server.entity.*;
+import net.minestom.server.entity.attribute.Attribute;
+import net.minestom.server.entity.attribute.AttributeModifier;
+import net.minestom.server.entity.attribute.AttributeOperation;
 import net.minestom.server.entity.damage.DamageType;
 import net.minestom.server.entity.metadata.display.BlockDisplayMeta;
 import net.minestom.server.instance.Instance;
 import net.minestom.server.instance.block.Block;
 import net.minestom.server.item.ItemStack;
 import net.minestom.server.item.Material;
+import net.minestom.server.item.component.AttributeList;
 import net.minestom.server.item.component.TooltipDisplay;
 import net.minestom.server.network.packet.server.play.ParticlePacket;
 import net.minestom.server.particle.Particle;
@@ -32,9 +33,9 @@ import java.util.function.Consumer;
 
 public record MaceWeapon() implements Weapon {
     public static final Tag<Boolean> IS_LAUNCH_ATTACK = Tag.Boolean("testgame.attack.mace.is_launch_attack");
-    public static final Tag<Double> SLAM_RADIUS = Tag.Double("testgame.attack.mace.slam_radius").defaultValue(0.0);
-    public static final Tag<Double> SLAM_INTENSITY = Tag.Double("testgame.attack.mace.slam_intensity").defaultValue(0.0);
-    public static final Tag<Double> SWING_RADIUS = Tag.Double("testgame.attack.mace.swing_radius").defaultValue(0.0);
+    public static final Tag<Double> SLAM_RADIUS = Tag.Double("testgame.attack.mace.slam_radius").defaultValue(2.5);
+    public static final Tag<Double> SLAM_INTENSITY = Tag.Double("testgame.attack.mace.slam_intensity").defaultValue(20.0);
+    public static final Tag<Double> SWING_RADIUS = Tag.Double("testgame.attack.mace.swing_radius").defaultValue(1.5);
 
     @Override
     public void attack(Attack attack, Consumer<Attack> next) {
@@ -44,9 +45,9 @@ public record MaceWeapon() implements Weapon {
         }
 
         if (attack.getTag(IS_LAUNCH_ATTACK)) {
-            attack.setTag(Attack.DAMAGE, 8.0);
+            attack.setTag(Attack.DAMAGE, 16.0);
         } else {
-            attack.setTag(Attack.DAMAGE, 12.0);
+            attack.setTag(Attack.DAMAGE, 14.0);
         }
 
         attack.setTag(Attack.COOLDOWN, 1.5);
@@ -66,10 +67,9 @@ public record MaceWeapon() implements Weapon {
             Instance instance = user.getInstance();
 
             if (attack.getTag(IS_LAUNCH_ATTACK)) {
-                double radius = 2.5 + attack.getTag(SLAM_RADIUS);
-                Collection<Entity> entities = instance.getNearbyEntities(user.getPosition(), radius);
+                Collection<Entity> entities = instance.getNearbyEntities(user.getPosition(), attack.getTag(SLAM_RADIUS));
                 
-                displayBlockVisuals(instance, user.getPosition().asVec(), (int) Math.round(radius));
+                displayBlockVisuals(instance, user.getPosition().asVec(), (int) Math.round(attack.getTag(SLAM_RADIUS)));
                 instance.sendGroupedPacket(new ParticlePacket(Particle.CAMPFIRE_COSY_SMOKE, false, true, user.getPosition(), new Vec(0, 0, 0), 0.1f, 100));
                 instance.playSound(Sound.sound(Key.key("minecraft:block.anvil.land"), Sound.Source.PLAYER, 0.5f, 0.5f), user.getPosition());
                 instance.playSound(Sound.sound(Key.key("minecraft:entity.dragon_fireball.explode"), Sound.Source.PLAYER, 0.8f, 1.0f), user.getPosition());
@@ -81,7 +81,7 @@ public record MaceWeapon() implements Weapon {
 
                     Entity target = mob.asEntity();
 
-                    Vec newVel = target.getVelocity().withY(target.getVelocity().y() + (20 + attack.getTag(SLAM_INTENSITY)));
+                    Vec newVel = target.getVelocity().withY(target.getVelocity().y() + attack.getTag(SLAM_INTENSITY));
                     target.setVelocity(newVel);
                     mob.applyAttack(DamageType.PLAYER_ATTACK, attack);
                 }
@@ -92,7 +92,7 @@ public record MaceWeapon() implements Weapon {
                 continue;
             }
 
-            Collection<Entity> entities = instance.getNearbyEntities(target.getPosition(), 1.5 + attack.getTag(SWING_RADIUS));
+            Collection<Entity> entities = instance.getNearbyEntities(target.getPosition(), attack.getTag(SWING_RADIUS));
             for (Entity entity : entities) {
                 if (!(entity instanceof AttackableMob e) || entity instanceof Player) {
                     continue;
@@ -148,6 +148,7 @@ public record MaceWeapon() implements Weapon {
     public ItemStack.Builder generateItem() {
         return ItemStack.builder(Material.MACE)
                 .set(DataComponents.ENCHANTMENT_GLINT_OVERRIDE, true)
+                .set(DataComponents.ATTRIBUTE_MODIFIERS, new AttributeList(new AttributeList.Modifier(Attribute.ENTITY_INTERACTION_RANGE, new AttributeModifier("testgame.weapons.mace.modifier", 5.0, AttributeOperation.ADD_VALUE), EquipmentSlotGroup.HAND)))
                 .set(DataComponents.TOOLTIP_DISPLAY, new TooltipDisplay(false, Set.of(DataComponents.ATTRIBUTE_MODIFIERS)))
                 .customName(ChatUtil.toComponent("&r&7&c&lMace"))
                 .lore(
